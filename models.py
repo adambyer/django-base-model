78141bc76b64d8c943106c2da5c7995f2be671ab
Adam Byer <byeradam@gmail.com>
2017-04-30 10:07:38 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.admin import ModelAdmin
 
 from hashids import Hashids
 
@@ -24,10 +25,12 @@ class BaseModelQuerySet(models.query.QuerySet):
         return super().get(**kwargs)
 
     def all(self):
+        # exclude is_deleted records
         return super().exclude(is_deleted=True)
 
     @hash_decorator
     def filter(self, *args, **kwargs):
+        # allow filtering by is_deleted if specified, otherwise exclude them
         if kwargs.get('is_deleted') is None and kwargs.get('is_deleted__exact') is None:
             kwargs.pop('is_deleted', None)
             kwargs['is_deleted__exact'] = False
@@ -36,6 +39,7 @@ class BaseModelQuerySet(models.query.QuerySet):
 
     @hash_decorator
     def exclude(self, *args, **kwargs):
+        # allow excluding by is_deleted if specified, otherwise exclude them
         if kwargs.get('is_deleted') is None and kwargs.get('is_deleted__exact') is None:
             kwargs.pop('is_deleted', None)
             kwargs['is_deleted__exact'] = False
@@ -66,24 +70,28 @@ class BaseModel(models.Model):
     class Meta:
         abstract = True
 
+    class Options:
+        fake_delete = False
+
     def __init__(self, *args, **kwargs):
         super(BaseModel, self).__init__(*args, **kwargs)
 
     objects = BaseModelManager()
     admin_objects = BaseAdminModelManager()
 
-    fake_delete = True
-
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
     is_deleted = models.BooleanField(default=False)
 
     def delete(self, *args, **kwargs):
-        if self.fake_delete:
+        if self.Options.fake_delete:
             self.is_deleted = True
             self.save()
             return 1
 
+        return super().delete(*args, **kwargs)
+
+    def hard_delete(self, *args, **kwargs):
         return super().delete(*args, **kwargs)
 
     @property
@@ -95,3 +103,29 @@ class BaseModel(models.Model):
         h = hashids.encode(self.pk)
 
         return h
+
+
+class BaseModelAdmin(ModelAdmin):
+    readonly_fields = ('created_at', 'modified_at')
+
+    list_display = ('created_at', 'is_deleted')
+
+    fieldsets = (
+        ('Base', {'fields': ('created_at', 'modified_at', 'is_deleted')}),
+    )
+
+    list_filter = ('is_deleted',)
+
+    def get_queryset(self, request):
+        """
+        this was taken directly from ModelAdmin:get_queryset
+        updated self.model._default_manager to self.model.admin_objects
+        to bypass BaseModel is_deleted functionality
+        """
+        qs = self.model.admin_objects.get_queryset()
+        ordering = self.get_ordering(request)
+
+        if ordering:
+            qs = qs.order_by(*ordering)
+
+        return qs
